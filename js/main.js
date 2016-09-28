b59fe03d6e07e7c7fbec79b784c1b81820c6e0f5
Amoury <ansar.memon@hotmail.com>
2016-09-28 15:57:26 +0400

    - Markers are working now.

--- js/main.js
+++ js/main.js
@@ -1,58 +1,53 @@
 // Step 1 - Initialize the map
-
 var map;
 
-// This Constructor function creates the new map (found on Google docs https://developers.google.com/maps/documentation/javascript/tutorial)
 
+// This Constructor function creates the new map (found on Google docs https://developers.google.com/maps/documentation/javascript/tutorial)
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 25.2711 , lng: 55.3075},
           zoom: 11,
           disableDefaultUI: true
         });
-      }
+    
 
+// Get the Data from an API
+	var count;
+	var locations;
+	var placesList = $('.places-list');
+	var placesObject;
+	var placesAPI = "http://tour-pedia.org/api/getPlaces?location=Dubai&category=attraction";
 
-
-// Get the Data
-var placesObject;
-
-var placesAPI = "http://tour-pedia.org/api/getPlaces?location=Dubai&category=attraction";
-
-// This IIFE requests the data from API
-
-(function(){
-	$.getJSON( placesAPI, function() {
-		console.log( "success" );
-	})
-	.done(function(data) {
-		var count = 0;
-		for(count = 0; count < 10; count++) {
-			placesObject = data[count] ;
-			placesObject = window.placesObject;
-
-			$('.places-list')
-			.append("<li>" + placesObject["name"] + "<br><span>" + placesObject["address"] + "</span></li>");
-
-			console.log(placesObject);
-
-
-		var markers = [];
-
-
-
-
-
+	$.getJSON( placesAPI, function(data) {
+		locations = data;		// Array of unlimited objects - Each object contains a place details
+		for(count = 0; count < 25; count++) {
+			// console.log(locations[count]["name"]);
+			placesList.append('<li>' + locations[count]["name"] + '<br><span>' + locations[count]["address"] + '</span></li>');
+			setMarkers();
+			
 		}
 	});
 
+	// Function that initializes the Markers (All Marker settings go in here)
+	var markersArray = [];
 
-})();
+	var setMarkers = function() {
+		marker = new google.maps.Marker({
+			map: map,
+			position: new google.maps.LatLng(locations[count]["lat"], locations[count]["lng"]),
+			title: locations[count]["name"],
+			animation: google.maps.Animation.DROP
+		});
+
+		markersArray.push(marker);
+	}
+
+	
+		
+// InfoWindow
+	
 
 
 
-
-
-
-
+}  // End of Function init()
 
